eb813d321f720de31de4c6610b380c08e5eb3e5f
Eduardo Nardone <eduardo.nardone@emplifi.com>
2024-09-22 14:15:07 +0200

    First commit

---
+++ first-install.sh
@@ -0,0 +1,15 @@
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.dotfiles/.fzf
+~/.dotfiles/.fzf/install
+rm -rf ~/.dotfiles/.fzf/.git
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.dotfiles/zsh-autosuggestions
+rm -rf ~/.dotfiles/zsh-autosuggestions/.git
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/zsh-syntax-highlighting
+rm -rf ~/.dotfiles/zsh-syntax-highlighting.git/.git
+
+echo "Set gnome terminal to use zsh"
+chsh -s $(which zsh)
+
+touch ~/.zshrc
+echo "echo "source ./.dotfiles/.zshrc" >> ~/.zshrc"
